89f473f8e214aa1f62e2a7742eff9463d287280a
HilgaRizkyNugraha <hilgarizkynugraha@gmail.com>
2024-01-02 19:57:29 +0700

    Submisiion 8

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\MyRepositoryController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RepositoryController;
+use App\Models\User;
+use Illuminate\Support\Facades\auth;
+use Illuminate\Support\Facades\Route;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('repository', RepositoryController::class)->middleware('auth');
+Route::resource('user', UserController::class)->middleware(['auth', 'can:isAdmin']);
+Route::get('/myrepositories', [MyRepositoryController::class, 'index'])->name('myrepo');
+
+
